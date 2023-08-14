781658864d321f04e08485afe3613f70a1d81e76
Jason E. Church <elijahchurch@gmail.com>
2023-08-14 10:21:28 -0700

    Seed some data into the Destination table Co-authored By: Heranan Verar hernanverar@gmail.com

---
+++ TravelAPI/Migrations/20230814171947_SeedData.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelAPI.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Destinations",
+                columns: new[] { "DestinationId", "CityName", "Country", "Rating", "Review" },
+                values: new object[,]
+                {
+                    { 1, "Bend", "United States", 9, "Nice sunny vaction spot" },
+                    { 2, "Sayulita", "Mexico", 8, "Surf vacation spot" },
+                    { 3, "Osaka", "Japan", 10, "Great place to see Japanese food spots and culture" },
+                    { 4, "Alicante", "Spain", 8, "Good food and great flamenco" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Destinations",
+                keyColumn: "DestinationId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Destinations",
+                keyColumn: "DestinationId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Destinations",
+                keyColumn: "DestinationId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Destinations",
+                keyColumn: "DestinationId",
+                keyValue: 4);
+        }
+    }
+}
--- TravelAPI/Models/TravelAPIContext.cs
+++ TravelAPI/Models/TravelAPIContext.cs
@@ -6,8 +6,21 @@ namespace TravelAPI.Models
     {
         public DbSet<Destination> Destinations { get; set; }
         public TravelAPIContext(DbContextOptions<TravelAPIContext> options) : base(options)
-    {
-    }
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Destination>()
+                .HasData(
+                    new Destination { DestinationId = 1, CityName = "Bend", Country = "United States", Review = "Nice sunny vaction spot", Rating = 9},
+                    new Destination { DestinationId = 2, CityName = "Sayulita", Country = "Mexico", Review = "Surf vacation spot", Rating = 8}, 
+                    new Destination { DestinationId = 3, CityName = "Osaka", Country="Japan", Review = "Great place to see Japanese food spots and culture", Rating = 10}, 
+                    new Destination { DestinationId = 4, CityName = "Alicante", Country="Spain", Review = "Good food and great flamenco", Rating = 8} 
+                );
+        }
+
+
 
     }
 }
