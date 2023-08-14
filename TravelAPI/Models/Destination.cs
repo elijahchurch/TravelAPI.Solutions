3fbd18ca3029d0d88747e41d0be4560ad7b69641
Jason E. Church <elijahchurch@gmail.com>
2023-08-14 14:58:14 -0700

    Add model validation attributes to Destination Co-authored By: Hernan Verar hernanverar@gmail.com

--- TravelAPI/Models/Destination.cs
+++ TravelAPI/Models/Destination.cs
@@ -1,12 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TravelAPI.Models
 {
   public class Destination
   {
     public int DestinationId {get; set; }
+    [Required]
+    [StringLength(20)]
     public string CityName { get; set; }
+    [Required]
     public string Country { get; set; }
     public string Review { get; set; }
+    [Required]
+    [Range(0, 10, ErrorMessage = "Must be 1 to 10 number")]
     public int Rating { get; set; }
-
   }
 }
